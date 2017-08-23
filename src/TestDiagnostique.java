7f72a820070034041a68a2323d397dd319f1907f
nadineducegep <st-amand.nadine@cgmatane.qc.ca>
2017-08-23 13:54:11 -0400

    Ajout des composants graphiques

--- src/TestDiagnostique.java
+++ src/TestDiagnostique.java
@@ -1,9 +1,47 @@
+import java.awt.Color;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TestDiagnostique {
 
 	public static void main(String[] args) 
 	{
-
+		
+		// Initialiser une fenetre
+		JFrame fenetre = new JFrame();
+		fenetre.setSize(100, 200);
+		JPanel panneauPrincipal = new JPanel();
+		panneauPrincipal.setLayout(new BoxLayout(panneauPrincipal, BoxLayout.Y_AXIS));
+		fenetre.setContentPane(panneauPrincipal);
+		
+		// Créer et ajouter les composants graphiques
+		JLabel libelleMontant = new JLabel("Montant (CAD)");
+		JTextField montant = new JTextField(7);
+		
+		panneauPrincipal.add(libelleMontant);
+		panneauPrincipal.add(montant);
+		
+		String[] listeDevises = {"EUR","USD","BTC"};
+		JComboBox selecteurDevises = new JComboBox(listeDevises);
+		panneauPrincipal.add(selecteurDevises);
+		
+		JButton actionConvertirDevise = new JButton("====>>>");
+		panneauPrincipal.add(actionConvertirDevise);
+		
+		JLabel resultatConversion = new JLabel("test");
+		resultatConversion.setOpaque(true);
+		resultatConversion.setBackground(Color.BLACK);
+		resultatConversion.setForeground(Color.WHITE);
+		panneauPrincipal.add(resultatConversion);
+		
+		// Afficher la fenetre
+		fenetre.setVisible(true);
 	}
 
 }
